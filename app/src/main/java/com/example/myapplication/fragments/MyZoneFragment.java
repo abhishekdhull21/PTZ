7a254ebea015dc8926813b4d7965e55d84b7431e
abhishekdhull21 <21jiabhi@gmail.com>
2021-06-21 15:24:57 +0530

    MyWallet added with design and fragment

--- app/src/main/java/com/example/myapplication/fragments/MyZoneFragment.java
+++ app/src/main/java/com/example/myapplication/fragments/MyZoneFragment.java
@@ -1,23 +1,37 @@
 package com.example.myapplication.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.myapplication.R;
+import com.example.myapplication.WalletActivity;
 
-public class MyZoneFragment extends Fragment {
+public class MyZoneFragment extends Fragment implements View.OnClickListener {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_my_zone, container, false);
+        View v =  inflater.inflate(R.layout.fragment_my_zone, container, false);
+        CardView walletView = v.findViewById(R.id.my_wallet);
+        walletView.setOnClickListener(this);
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.my_wallet:
+                startActivity(new Intent(getActivity(),WalletActivity.class));
+        }
     }
 }
